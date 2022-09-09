284ddfb7ba2729c6a730b014c75db328ccd14bfc
Amiemar <ayoabiolamariam@gmail.com>
2022-09-09 15:50:31 -0700

    all done

---
+++ 7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - output lower case characters of the english
+ * language in reverse followed by a new line
+ * Return: 0 if (successful)
+ */
+int main(void)
+{
+		char ch;
+		for (ch = 'z'; ch >= 'a'; ch--)
+				{
+							putchar(ch);
+								}
+
+			putchar('\n');
+				return (0);
+}
