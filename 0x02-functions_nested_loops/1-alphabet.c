0f402e652af6fc42175f8d1fbc4b01485475c54e
Amiemar <ayoabiolamariam@gmail.com>
2022-09-13 16:54:56 -0700

    second correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,15 @@
  *
  *  Description: print all the letters of the english language from a to z then a new line. The rule is that the code can only contain the _putchar function twice characters contained in _putchar
  *
- *  Return: always 0
+ *  Return: always 0 (Success)
  */
 void print_alphabet(void)
 {
-	char j;
+	int i;
 
-	for (j = 'a'; j <= 'z'; j++)
+	for (i = 97; i <= 122; i++)
 	{
-		_putchar(j);
+		_putchar(i);
 	}
 	_putchar('\n');
 }
